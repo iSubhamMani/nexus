62fe54f636b56b876909e96105cc843994874c5a
Subham Mani <manisubham09@gmail.com>
2024-04-30 21:24:41 +0530

    Add ThemeToggler component

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AvatarSection from "./AvatarSection";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import ThemeToggler from "./ThemeToggler";
 
 const words = "My social life compiles better on weekends";
 
@@ -8,6 +9,7 @@ function IntroSection() {
   return (
     <div>
       <AvatarSection />
+      <ThemeToggler />
       <div className="text-center px-6">
         <TextGenerateEffect words={words} />
       </div>
