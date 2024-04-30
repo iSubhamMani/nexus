b613cbaf8ba959b92d2853b043a600c16c82f24a
Subham Mani <manisubham09@gmail.com>
2024-04-30 21:24:49 +0530

    Update SocialButton styling

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -14,7 +14,7 @@ function SocialButton({
   return (
     <button
       onClick={onClickHandler}
-      className="font-medium hover:bg-black hover:text-white hover:border-white dark:hover:bg-white dark:hover:text-black dark:hover:border-black border-2 border-black dark:border-white uppercase bg-white dark:bg-black text-black dark:text-white transition duration-200 text-[1rem] shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+      className="font-medium hover:bg-black hover:text-green-500 hover:border-white dark:hover:bg-white dark:hover:text-green-600 dark:hover:border-black border-2 border-black dark:border-white uppercase bg-white dark:bg-black text-black dark:text-white transition duration-200 text-[1rem] shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
     >
       <a
         href={link}
